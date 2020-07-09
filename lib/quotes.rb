a340cf590a91bbdaf041b18fafb33f21f61fcf09
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-07-09 21:45:47 +0000

    refactored code

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -1,6 +1,7 @@
 class QuoteOfTheDay::Quotes
   
   attr_accessor :title, :content
+    @@categories = ["inspire", "management", "sports", "life", "funny", "love", "art", "students"] 
 
  def self.today
     self.scrape_quotes
@@ -9,21 +10,15 @@ class QuoteOfTheDay::Quotes
  def self.scrape_quotes
     quotes = []
   
-   quotes << scrape_first_quote
-   quotes << scrape_second_quote
-   quotes << scrape_third_quote
-   quotes << scrape_fourth_quote
-   quotes << scrape_fifth_quote
-   quotes << scrape_sixth_quote
-   quotes << scrape_seventh_quote
-   quotes << scrape_eighth_quote
-   
-    quotes
+  @@categories.each do |category|
+    quotes << self.scrape_quote(category)
+    end
+  quotes
   end
- 
   
-  def self.scrape_first_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/inspire#"))
+ 
+  def self.scrape_quote(url)
+  doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/" + url)) 
    
    quote = self.new
    quote.title = doc.search("h1").text
@@ -32,76 +27,5 @@ class QuoteOfTheDay::Quotes
    quote
   end
   
-
-  def self.scrape_second_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/management#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-    
-   quote
-  end
- 
-  def self.scrape_third_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/sports#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
-
-
-  def self.scrape_fourth_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/life#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
- 
- 
-  def self.scrape_fifth_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/funny#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
- 
-  def self.scrape_sixth_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/love#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
- 
-  def self.scrape_seventh_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/art#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
- 
-  def self.scrape_eighth_quote
-   doc = Nokogiri::HTML(open("https://theysaidso.com/quote-of-the-day/students#"))
-   
-   quote = self.new
-   quote.title = doc.search("h1").text
-   quote.content = doc.search("div.qcontent").text
-   
-   quote
-  end
+  
 end
